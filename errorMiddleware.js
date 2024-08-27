f1b8a664920a60d39042195ea3cb4c3f904306a0
vijayalakshmig1 <130355111+vijayalakshmig1@users.noreply.github.com>
2024-08-27 11:28:06 +0530

    Create errorMiddleware.js

---
+++ errorMiddleware.js
@@ -0,0 +1,10 @@
+const errorMiddleware = (err, req, res, next) => {
+    console.log(err)
+    res.status(500).send({
+        success: false,
+        message: "Something Went Wrong",
+        err,
+    });
+}
+
+export default errorMiddleware; 
